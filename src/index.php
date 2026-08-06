cd80dd09744aa060ea6348e36e2b090d58c3224e
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-05 21:57:28 -0300

    todo:  instruções para prática.

--- src/index.php
+++ src/index.php
@@ -10,6 +10,9 @@ Controller::ola();
 echo "<br>";
 var_dump(Controller::class);
 
+//TODO: Criar uma classe para ler e imprimir o valor do ENV.
+// Implementar como método estático
+// A classe deve estar em um diretório diferente da controllers
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
