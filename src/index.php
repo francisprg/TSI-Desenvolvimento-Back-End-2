57d46101d09709ba6fc19d55c7312972597f4d5a
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-05 21:26:28 -0300

    feat: usando a dependência do Dotenv

--- src/index.php
+++ src/index.php
@@ -3,8 +3,14 @@
 require '../vendor/autoload.php';
 
 use CSTSI\Dbe2\controllers\Controller;
+use Dotenv\Dotenv;
 
 
 Controller::ola();
 echo "<br>";
 var_dump(Controller::class);
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+echo "<br>Conectar no banco de nome: ".$_ENV['DB_NAME'];
