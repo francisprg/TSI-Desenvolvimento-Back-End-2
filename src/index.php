89c410e394619715aa88cf34e9fc9c985ba40ee7
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-12 19:44:06 -0300

    refactor: utiliza o método estático da nova classe

--- src/index.php
+++ src/index.php
@@ -3,17 +3,10 @@
 require '../vendor/autoload.php';
 
 use CSTSI\Dbe2\controllers\Controller;
-use Dotenv\Dotenv;
-
+use CSTSI\Dbe2\core\App;
 
 Controller::ola();
 echo "<br>";
 var_dump(Controller::class);
 
-//TODO: Criar uma classe para ler e imprimir o valor do ENV.
-// Implementar como método estático
-// A classe deve estar em um diretório diferente da controllers
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-echo "<br>Conectar no banco de nome: ".$_ENV['DB_NAME'];
+App::handleEnv();
