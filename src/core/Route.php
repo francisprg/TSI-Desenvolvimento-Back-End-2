1a83e54c09ff07966f374a45742b542c6e9faafc
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-13 11:30:47 -0300

    feat: classe para tratamento de rotas da URL A classe segue o padrão /controlador/método/parâmetro. Ela converte a string da url em um vetor de três posições, sendo a primeira o controlador. O controlador deve ser mapeado no arquivo de configuração de rotas.

---
+++ src/core/Route.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace CSTSI\Dbe2\core;
+
+use CSTSI\Dbe2\controllers\Controller;
+// use CSTSI\Dbe2\app\views\View;
+
+class Route
+{
+
+    // localhost:porto/controlers/method/param
+    public static function resolve(array $routes): void {
+        $uriQuery = self::parseURI();
+
+        $class = null;
+		$method = null;
+		$param = null;
+		
+		if ($uriQuery) {
+			$class_name = $uriQuery[0];
+			if (count($uriQuery) > 1) {
+				$method = $uriQuery[1];
+				$param = (count($uriQuery) > 2) ? $uriQuery[2] : null;
+			}
+
+			if (isset($routes[$class_name])) {
+				$class = new $routes[$class_name];//produtos
+				if ($class instanceof Controller) {
+					if ($method && method_exists($class, $method)) {
+						if ($param) {
+							$class->$method($param);
+						} else {
+							$class->$method();
+						}
+					} else {
+						if (method_exists($class, 'index'))
+							$class->index();
+						else $class = null;
+					}
+				}
+			}
+		}
+		// if (!$class) View::pageNotFound();
+		if (!$class) header('HTTP/1.0 404 Not Found');
+    }
+
+    private static function parseURI(): array
+    {
+        if ($_SERVER['REQUEST_URI'] == '/') {
+            return [$_SERVER['REQUEST_URI']];
+        } else {
+            $url_path = trim($_SERVER['REQUEST_URI'], '/');
+            error_log("ROUTE: $url_path");
+            return explode('/', $url_path);
+        }
+    }
+}
