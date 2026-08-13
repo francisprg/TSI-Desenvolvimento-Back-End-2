a084821dc077b8804568235cc4bfc666052932f5
Prof. Gill <gillgonzales@ifsul.edu.br>
2026-08-13 11:35:19 -0300

    refactor: modificação para a inicialização da aplicação.

--- src/index.php
+++ src/index.php
@@ -2,11 +2,6 @@
 
 require '../vendor/autoload.php';
 
-use CSTSI\Dbe2\controllers\Controller;
 use CSTSI\Dbe2\core\App;
 
-Controller::ola();
-echo "<br>";
-var_dump(Controller::class);
-
-App::handleEnv();
+App::init();
